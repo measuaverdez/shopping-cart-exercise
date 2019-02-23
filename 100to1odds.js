130edac7b697c933dc6fd3620675922320e58dfe
measuaverdez <measuaverdez@trepity.co>
2019-02-23 22:12:46 +0800

    odds

--- 100to1odds.js
+++ 100to1odds.js
@@ -1,8 +1,9 @@
 //100 to 1 odds
 function hundredToOne(){
     for(var i=100; i >= 0; i--){
-        if(i%2 === 1)
-        console.log(i)
+        if(i%2 === 1){
+            console.log(i)
+        }
     }
 }
 var hundredToOne = new hundredToOne()
