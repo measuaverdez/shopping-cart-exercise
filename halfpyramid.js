4c116ac833e98e573aaeb1e2441d61d6f6d05798
measuaverdez <measuaverdez@trepity.co>
2019-02-23 22:34:04 +0800

    halfpyramid

---
+++ halfpyramid.js
@@ -0,0 +1,11 @@
+function halfpyramid(){
+    
+        for(var i=1; i <= 10; i++){
+            var num = ""
+            for(var j=1 ; j<=i ; j++){
+                num += "*"
+           }
+           console.log(num)
+        }
+}
+halfpyramid()
